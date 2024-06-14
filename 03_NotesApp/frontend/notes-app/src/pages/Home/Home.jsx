a164f39a58e0a7ed5c403cb1bcc81c549514b123
RekyGeorge <rekygeorgephilip.f@northeastern.edu>
2024-06-13 22:59:03 -0400

    Frontend Changes

--- 03_NotesApp/frontend/notes-app/src/pages/Home/Home.jsx
+++ 03_NotesApp/frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import NoteCard from '../../components/Cards/NoteCard';
+import {MdAdd} from'react-icons/md';
+import AddEditNotes from './AddEditNotes';
+import Modal from 'react-modal';
 
 const Home = () => {
+
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShow : false,
+    type: "add",
+    data: null,
+  })
+
+
+
   return (
-    <div>Home</div>
+    <>
+      <Navbar/>
+
+      <div className='container mx-auto'>
+        <div className=' grid grid-cols-3 gap-4 mt-8'>
+          <NoteCard title="Meeting on 7th April"  
+            date="3rd Apr 2024"
+            content = "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+            tags = "#Meeting"
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+
+        </div>
+      </div>
+
+      <button 
+        className='w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10 ' 
+        onClick={() => {
+          setOpenAddEditModal({
+            isShow: true,
+            type: "add",
+            data: null,
+          });
+        }}>
+          <MdAdd className= 'text-[32px] text-white'/>
+      </button>
+        
+      <Modal
+        isOpen={openAddEditModal.isShow}
+        onRequestClose={() => {}}
+        style={{
+          overlay:{
+            backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          },
+        }}
+        contentLabel=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll "
+      >
+
+
+        <AddEditNotes />
+      </Modal>
+
+    </>
   )
 }
 
--- 03_NotesApp/frontend/notes-app/tailwind.config.js
+++ 03_NotesApp/frontend/notes-app/tailwind.config.js
@@ -5,11 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-    // Colors used in the project.
-    colors: {
-      'primary': '#2B85FF',
-     'secondary': '#EF863E',
+    extend: {
+      // Colors used in the project.
+      colors: {
+        'primary': '#2B85FF',
+      'secondary': '#EF863E',
+      
+        slate: {
+          400: '#94a3b8', // Example hex value
+        },
+      },
     },
   },
   plugins: [],
